4b7e480fbd98535921555035f6ca0a73cb692a6a
yetaoxu <yetaoxu@gmail.com>
2019-04-08 19:13:16 +0800

    指令对大小写不敏感

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 #include <zconf.h>
 #include <stdio.h>
 
-const int BUF_SIZE=1024;
+const int BUF_SIZE = 1024;
 
 int main() {
     int fd = connectToSever();
-    char closeStr[10] = "CLOSE";
+    char closeS[10] = "Bye Bye";
     while (true) {
         printf(">>");
         char order[BUF_SIZE];
@@ -15,11 +15,10 @@ int main() {
         sendOrder(fd, order);
         char *reponse = recvOrder(fd);
         printf("%s\n", reponse);
-        if (strcmp(order, closeStr) == 0) {
+        if (strcmp(reponse, closeS) == 0) {
             close(fd);
             break;
         }
     }
     return 0;
 }
-
